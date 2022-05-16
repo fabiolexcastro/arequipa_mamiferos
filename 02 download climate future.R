34867572c607e30db334dc2220dcaabb0755e8d0
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-16 17:56:10 -0500

    Climate future

---
+++ 02 download climate future.R
@@ -0,0 +1,17 @@
+
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(terra, sf, crayon, geodata, fs, tidyverse, glue)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999)
+
+# Download ----------------------------------------------------------------
+ssps <- c('ssp370')
+prdo <- '2081-2100'
+mdls <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR", "BCC-CSM2-MR", "CanESM5", "CanESM5-CanOE", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-CM6-1-HR", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3-Veg-LR", "FIO-ESM-2-0", "GFDL-ESM4", "GISS-E2-1-G", "GISS-E2-1-H", "HadGEM3-GC31-LL", "INM-CM4-8", "INM-CM5-0", "IPSL-CM6A-LR", "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MPI-ESM1-2-LR", "MRI-ESM2-0", "UKESM1-0-LL")
+base <- 'https://geodata.ucdavis.edu/cmip6/tiles/'
+
