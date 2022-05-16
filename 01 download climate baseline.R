2430a122082e5c098a2632a9c77121ce5188e4a1
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-16 17:21:13 -0500

    update

--- 01 download climate baseline.R
+++ 01 download climate baseline.R
@@ -2,7 +2,7 @@
 
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(terra, sf, fs, tidyverse, glue)
+pacman::p_load(terra, sf, crayon, geodata, fs, tidyverse, glue)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
@@ -12,6 +12,49 @@ options(scipen = 999)
 
 cntr <- 'PER'
 dir.create('../tmpr')
-limt <- geodata::gadm(country = cntr, path = '../tmpr')
+limt <- geodata::gadm(country = cntr, level = 0, path = '../tmpr')
+
+# To download 
+vars <- c('prec', 'tmax', 'tmin', 'tavg', 'bio')
+
+purrr::map(.x = 1:length(vars), .f = function(i){
+  
+  cat(bgGreen('Start ', i, '\n'))
+  var <- vars[i]
+  
+  rstr <- geodata::worldclim_country(country = cntr,
+                                     var = var,
+                                     path = '../tmpr')
+  
+  rstr <- 
+  
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
