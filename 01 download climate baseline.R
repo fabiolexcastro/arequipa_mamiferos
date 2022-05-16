0e3d60bd524afa94decfc724b02a303efe6ae64b
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-16 17:13:40 -0500

    update

--- 01 download climate baseline.R
+++ 01 download climate baseline.R
@@ -3,3 +3,15 @@
 # Load libraries ----------------------------------------------------------
 require(pacman)
 pacman::p_load(terra, sf, fs, tidyverse, glue)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999)
+
+# Download ----------------------------------------------------------------
+
+cntr <- 'PER'
+dir.create('../tmpr')
+limt <- geodata::gadm(country = cntr, path = '../tmpr')
+
+
