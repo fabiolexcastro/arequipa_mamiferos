b80b7d79dd22f20712d10f960c34fa32c69a82fb
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-23 17:08:01 -0500

    update

--- 02 download climate future.R
+++ 02 download climate future.R
@@ -15,3 +15,14 @@ prdo <- '2081-2100'
 mdls <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR", "BCC-CSM2-MR", "CanESM5", "CanESM5-CanOE", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-CM6-1-HR", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3-Veg-LR", "FIO-ESM-2-0", "GFDL-ESM4", "GISS-E2-1-G", "GISS-E2-1-H", "HadGEM3-GC31-LL", "INM-CM4-8", "INM-CM5-0", "IPSL-CM6A-LR", "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MPI-ESM1-2-LR", "MRI-ESM2-0", "UKESM1-0-LL")
 base <- 'https://geodata.ucdavis.edu/cmip6/tiles/'
 
+# Limit
+cntr <- 'PER'
+dir.create(cntr)
+limt <- geodata::gadm(country = cntr, level = 0, path = '../tmpr')
+plot(limt)
+
+# To apply the function ---------------------------------------------------
+
+
+
+
