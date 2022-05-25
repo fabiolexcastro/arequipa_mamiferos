4a77370787709a26ddd4984109f9ce1e75eb27cb
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-05-25 17:02:49 -0500

    update

--- 02 download climate future.R
+++ 02 download climate future.R
@@ -11,7 +11,7 @@ options(scipen = 999)
 
 # Download ----------------------------------------------------------------
 ssps <- c('ssp370')
-prdo <- '2081-2100'
+prds <- '2081-2100'
 mdls <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR", "BCC-CSM2-MR", "CanESM5", "CanESM5-CanOE", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-CM6-1-HR", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3-Veg-LR", "FIO-ESM-2-0", "GFDL-ESM4", "GISS-E2-1-G", "GISS-E2-1-H", "HadGEM3-GC31-LL", "INM-CM4-8", "INM-CM5-0", "IPSL-CM6A-LR", "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MPI-ESM1-2-LR", "MRI-ESM2-0", "UKESM1-0-LL")
 base <- 'https://geodata.ucdavis.edu/cmip6/tiles/'
 vars <- c('prec', 'tmax', 'tmin')
@@ -35,7 +35,7 @@ prd <- prdo[1]
 var <- vars[1]
 
 # Loop
-purrr::map(.x = 1:lenth(ssp), .f = function(i){
+purrr::map(.x = 1:length(ssp), .f = function(i){
   
   purrr::map(.x = 1:length(prdo), .f = function(j){
     
