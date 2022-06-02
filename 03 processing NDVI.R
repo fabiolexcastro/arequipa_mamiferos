39b342c1a33d23c202c3a0d5067f1cbeb111e143
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-02 07:18:57 -0500

    update

--- 03 processing NDVI.R
+++ 03 processing NDVI.R
@@ -7,3 +7,7 @@ pacman::p_load(terra, sf, crayon, geodata, fs, tidyverse, glue)
 g <- gc(reset = TRUE)
 rm(list = ls())
 options(scipen = 999)
+
+# Load data ---------------------------------------------------------------
+path <- '../ndvi_clases'
+fles <- dir_ls(path, regexp = '.tif$')
