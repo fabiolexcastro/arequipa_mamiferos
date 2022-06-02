fe61cb66357a2e8249a6202b2a4b6f51177d65e3
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-02 07:16:41 -0500

    update

---
+++ 03 processing NDVI.R
@@ -0,0 +1,9 @@
+
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(terra, sf, crayon, geodata, fs, tidyverse, glue)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999)
