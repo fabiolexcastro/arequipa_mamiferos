73a91feea1c3595958e5fb856d289e593439d8db
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-04 09:11:30 -0500

    100 meters resampling process

---
+++ 04 make resample to 100 meters.R
@@ -0,0 +1,12 @@
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(terra, sf, crayon, geodata, fs, tidyverse, glue)
+
+g <- gc(reset = TRUE)
+rm(list = ls())
+options(scipen = 999)
+
+# Load data ---------------------------------------------------------------
+
+
