869b16bcea949a7ef5c703850607d4e16c85fbb5
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-04 09:30:13 -0500

    update

--- 04 make resample to 100 meters.R
+++ 04 make resample to 100 meters.R
@@ -17,7 +17,7 @@ mask <- rstr[[1]] * 0
 # Create a empty raster ---------------------------------------------------
 resolution <- 0.00083333
 ext <- terra::extent(mask)
-naraster <- terra::rast(terra::extent(mask), ncols = (diff(ext[1:2]/resolution)), nrow = (diff(ext[3:4]/resolution)))
+naraster <- terra::rast(terra::ext(mask), ncols = (diff(ext[1:2]/resolution)), nrow = (diff(ext[3:4]/resolution)))
 naraster[] <- 1
 naraster
 
